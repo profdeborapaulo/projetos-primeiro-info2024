b41d94ce8449ab428c97827db747b0eec04f62c0
PiettroXavier <piettroxavier2016@gmail.com>
2024-09-19 18:44:48 -0300

    atualização dos arquivos

--- projeto-empresa-asas/Piettro Xavier/Asas.js
+++ projeto-empresa-asas/Piettro Xavier/Asas.js
@@ -2,93 +2,103 @@ let regiao = "norte" /* Seleciona a Região Para Qual Será Realizada a Viagem *
 let criancaeconomica = 4 /* Indica Quantas Crianças Irão Viajar na Classe Econômica */
 let adultoeconomica = 1 /* Indica Quantos Adultos Irão Viajar na Classe Econômica */
 let criancaexecutiva = 3 /* Indica Quantas Crianças Irão Viajar na Classe Executiva */
-let adultoexecutiva = 2 /* Indica Quantaos Adultos Irão Viajar na Classe Executiva */
-
+let adultoexecutiva = 2 /* Indica Quantos Adultos Irão Viajar na Classe Executiva */
+let totaladulto = adultoeconomica + adultoexecutiva /* Indica Quantos Adultos Irão Viajar */
+let totalcrianca = criancaeconomica + criancaexecutiva /* Indica Quantas Crianças Irão Viajar */
+let totaleconomica = criancaeconomica + adultoeconomica /* Indica Quantas Pessoas Irão Na Classe Econômica */
+let totalexecutiva = criancaexecutiva + adultoexecutiva /* Indica Quantas Pessoas Irão Na Classe Executiva */
+ 
 if (regiao == "norte") { /* Determina Condição Caso A Região Seja O Norte do Brasil */
+let total = (criancaeconomica * 1800) + (adultoeconomica * 2500) + (criancaexecutiva * (1800 + 1800 * 10/100)) + (adultoexecutiva * (2500 + 2500 + 20/100)) /* Calcula o Valor TOtal Da Viagem  */
 console.log ()
-console.log ("A quantidade de Passageiros Adultos é:", adultoeconomica + adultoexecutiva) /* Calcula a Quantidade de Passageiras Adultos */
+console.log ("A quantidade de Passageiros Adultos é:", totaladulto) /* Quantidade de Passageiras Adultos */
 console.log ()
-console.log ("A quantidade de Passageiros Crianças é:", criancaeconomica + criancaexecutiva) /* Calcula a Quantidade de Passageiras Crianças */
+console.log ("A quantidade de Passageiros Crianças é:", totalcrianca) /* Quantidade de Passageiras Crianças */
 console.log ()
 console.log ("Destino escolhido:", regiao) /* Exibe Para onde Será a Viagem */
 console.log ()
-console.log ("A quantidade de pessoas na classe econômica é:", criancaeconomica + adultoeconomica) /* Calcula a Quantidade de Passageiras Na Classe Econômica */
-console.log ("Sendo:", criancaeconomica, "criança(s) &", adultoeconomica, "adulto(s)") /* Exibe a Quantidade de Adultos e Crianças Separadamente*/
+console.log ("A quantidade de pessoas na classe econômica é:", totaleconomica) /* Quantidade de Passageiras Na Classe Econômica */
+console.log ("Sendo:", criancaeconomica, "criança(s) &", adultoeconomica, "adulto(s)") /* Quantidade de Adultos e Crianças Separadamente*/
 console.log ()
-console.log ("A quantidade de pessoas na classe executiva é:", criancaexecutiva + adultoexecutiva) /* Calcula a Quantidade de Passageiras Na Classe Executiva */
-console.log ("Sendo:", criancaexecutiva, "criança(s) &", adultoexecutiva, "adulto(s)") /* Exibe a Quantidade de Adultos e Crianças Separadamente*/
+console.log ("A quantidade de pessoas na classe executiva é:", totalexecutiva) /* Quantidade de Passageiras Na Classe Executiva */
+console.log ("Sendo:", criancaexecutiva, "criança(s) &", adultoexecutiva, "adulto(s)") /* Quantidade de Adultos e Crianças Separadamente*/
 console.log ()
-console.log ("o valor total da viagem é: R$", (criancaeconomica * 1800) + (adultoeconomica * 2500) + (criancaexecutiva * (1800 + 1800 * 10/100)) + (adultoexecutiva * (2500 + 2500 + 20/100))) /*Calcula o Valor Total da Viagem*/
+console.log ("o valor total da viagem é: R$", total) /* Valor Total da Viagem*/
 console.log ()
 }
 
-else if (regiao == "sul") { /* Determina Condição Caso A Região Seja O Sul do Brasil */
+else if (regiao == "sul") { /* Determina Condição Caso A Região Seja O Norte do Brasil */
+let total = ((criancaeconomica + adultoeconomica) * 3000) + ((criancaexecutiva + adultoexecutiva) * 4000) /* Calcula o Valor TOtal Da Viagem  */
 console.log ()
-console.log ("A quantidade de Passageiros Adultos é:", adultoeconomica + adultoexecutiva) /* Calcula a Quantidade de Passageiras Crianças */
+console.log ("A quantidade de Passageiros Adultos é:", totaladulto) /* Quantidade de Passageiras Adultos */
 console.log ()
-console.log ("A quantidade de Passageiros Crianças é:", criancaeconomica + criancaexecutiva)  /* Calcula a Quantidade de Passageiras Crianças */
-console.log ()
-console.log ("Destino escolhido:", regiao) /* Exibe Para onde Será a Viagem */
-console.log ()
-console.log ("A quantidade de pessoas na classe econômica é:", criancaeconomica + adultoeconomica) /* Calcula a Quantidade de Passageiras Na Classe Econômica */
-console.log ("Sendo:", criancaeconomica, "criança(s) &", adultoeconomica, "adulto(s)") /* Exibe a Quantidade de Adultos e Crianças Separadamente */
-console.log ()
-console.log ("A quantidade de pessoas na classe executiva é:", criancaexecutiva + adultoexecutiva) /* Calcula a Quantidade de Passageiras Na Classe Executiva */
-console.log ("Sendo:", criancaexecutiva, "criança(s) &", adultoexecutiva, "adulto(s)") /* Exibe a Quantidade de Adultos e Crianças Separadamente */
-console.log ()
-console.log ("o valor total da viagem é: R$", ((criancaeconomica + adultoeconomica) * 3000) + ((criancaexecutiva + adultoexecutiva) * 4000)) /* Calcula o Valor Total da Viagem */
-}
-
-else if (regiao == "asia") { /* Determina Condição Caso A Viagem Seja Para a Ásia */
-console.log ()
-console.log ("A quantidade de Passageiros Adultos é:", adultoeconomica + adultoexecutiva) /* Calcula a Quantidade de Passageiras Crianças */
-console.log ()
-console.log ("A quantidade de Passageiros Crianças é:", criancaeconomica + criancaexecutiva) /* Calcula a Quantidade de Passageiras Crianças */
+console.log ("A quantidade de Passageiros Crianças é:", totalcrianca) /* Quantidade de Passageiras Crianças */
 console.log ()
 console.log ("Destino escolhido:", regiao) /* Exibe Para onde Será a Viagem */
 console.log ()
-console.log ("A quantidade de pessoas na classe econômica é:", criancaeconomica + adultoeconomica) /* Calcula a Quantidade de Passageiras Na Classe Econômica */
-console.log ("Sendo:", criancaeconomica, "criança(s) &", adultoeconomica, "adulto(s)") /* Exibe a Quantidade de Adultos e Crianças Separadamente */
+console.log ("A quantidade de pessoas na classe econômica é:", totaleconomica) /* Quantidade de Passageiras Na Classe Econômica */
+console.log ("Sendo:", criancaeconomica, "criança(s) &", adultoeconomica, "adulto(s)") /* Quantidade de Adultos e Crianças Separadamente*/
 console.log ()
-console.log ("A quantidade de pessoas na classe executiva é:", (criancaexecutiva + adultoexecutiva)) /* Calcula a Quantidade de Passageiras Na Classe Executiva */
-console.log ("Sendo:", criancaexecutiva, "criança(s) &", adultoexecutiva, "adulto(s)") /* Exibe a Quantidade de Adultos e Crianças Separadamente */
+console.log ("A quantidade de pessoas na classe executiva é:", totalexecutiva) /* Quantidade de Passageiras Na Classe Executiva */
+console.log ("Sendo:", criancaexecutiva, "criança(s) &", adultoexecutiva, "adulto(s)") /* Quantidade de Adultos e Crianças Separadamente*/
 console.log ()
-console.log ("o valor total da viagem é: R$", ((criancaeconomica + adultoeconomica) * 4600) + ((criancaexecutiva + adultoexecutiva) * 6000)) /* Calcula o Valor Total da Viagem */
+console.log ("o valor total da viagem é: R$", total) /* Valor Total da Viagem*/
 console.log ()
 }
 
-else if (regiao == "europa") { /* Determina Condição Caso A Viagem Seja Para a Europa */
+else if (regiao == "asia") { /* Determina Condição Caso A Região Seja O Norte do Brasil */
+let total = ((criancaeconomica + adultoeconomica) * 4600) + ((criancaexecutiva + adultoexecutiva) * 6000) /* Calcula o Valor TOtal Da Viagem  */
 console.log ()
-console.log ("A quantidade de Passageiros Adultos é:", adultoeconomica + adultoexecutiva) /* Calcula a Quantidade de Passageiras Crianças */
+console.log ("A quantidade de Passageiros Adultos é:", totaladulto) /* Quantidade de Passageiras Adultos */
 console.log ()
-console.log ("A quantidade de Passageiros Crianças é:", criancaeconomica + criancaexecutiva) /* Calcula a Quantidade de Passageiras Crianças */
+console.log ("A quantidade de Passageiros Crianças é:", totalcrianca) /* Quantidade de Passageiras Crianças */
 console.log ()
 console.log ("Destino escolhido:", regiao) /* Exibe Para onde Será a Viagem */
 console.log ()
-console.log ("A quantidade de pessoas na classe econômica é:", criancaeconomica + adultoeconomica) /* Calcula a Quantidade de Passageiras Na Classe Econômica */
-console.log ("Sendo:", criancaeconomica, "criança(s) &", adultoeconomica, "adulto(s)") /* Exibe a Quantidade de Adultos e Crianças Separadamente */
+console.log ("A quantidade de pessoas na classe econômica é:", totaleconomica) /* Quantidade de Passageiras Na Classe Econômica */
+console.log ("Sendo:", criancaeconomica, "criança(s) &", adultoeconomica, "adulto(s)") /* Quantidade de Adultos e Crianças Separadamente*/
 console.log ()
-console.log ("A quantidade de pessoas na classe executiva é:", criancaexecutiva + adultoexecutiva) /* Calcula a Quantidade de Passageiras Na Classe Executiva */
-console.log ("Sendo:", criancaexecutiva, "criança(s) &", adultoexecutiva, "adulto(s)") /* Exibe a Quantidade de Adultos e Crianças Separadamente */
+console.log ("A quantidade de pessoas na classe executiva é:", totalexecutiva) /* Quantidade de Passageiras Na Classe Executiva */
+console.log ("Sendo:", criancaexecutiva, "criança(s) &", adultoexecutiva, "adulto(s)") /* Quantidade de Adultos e Crianças Separadamente*/
 console.log ()
-console.log ("o valor total da viagem é: R$", ((criancaeconomica + adultoeconomica) * 5800) + ((criancaexecutiva + adultoexecutiva) * 7300)) /* Calcula o Valor Total da Viagem */
+console.log ("o valor total da viagem é: R$", total) /* Valor Total da Viagem*/
 console.log ()
 }
 
-else if (regiao == "africa") { /* Determina Condição Caso A Viagem Seja Para a África */
+else if (regiao == "europa") { /* Determina Condição Caso A Região Seja O Norte do Brasil */
+let total = ((criancaeconomica + adultoeconomica) * 5800) + ((criancaexecutiva + adultoexecutiva) * 7300) /* Calcula o Valor TOtal Da Viagem  */
 console.log ()
-console.log ("A quantidade de Passageiros Adultos é:", adultoeconomica + adultoexecutiva) /* Calcula a Quantidade de Passageiras Crianças */
+console.log ("A quantidade de Passageiros Adultos é:", totaladulto) /* Quantidade de Passageiras Adultos */
 console.log ()
-console.log ("A quantidade de Passageiros Crianças é:", criancaeconomica + criancaexecutiva) /* Calcula a Quantidade de Passageiras Crianças */
+console.log ("A quantidade de Passageiros Crianças é:", totalcrianca) /* Quantidade de Passageiras Crianças */
 console.log ()
 console.log ("Destino escolhido:", regiao) /* Exibe Para onde Será a Viagem */
 console.log ()
-console.log ("A quantidade de pessoas na classe econômica é:", criancaeconomica + adultoeconomica) /* Calcula a Quantidade de Passageiras Na Classe Econômica */
-console.log ("Sendo:", criancaeconomica, "criança(s) &", adultoeconomica, "adulto(s)") /* Exibe a Quantidade de Adultos e Crianças Separadamente */
+console.log ("A quantidade de pessoas na classe econômica é:", totaleconomica) /* Quantidade de Passageiras Na Classe Econômica */
+console.log ("Sendo:", criancaeconomica, "criança(s) &", adultoeconomica, "adulto(s)") /* Quantidade de Adultos e Crianças Separadamente*/
 console.log ()
-console.log ("A quantidade de pessoas na classe executiva é:", criancaexecutiva + adultoexecutiva) /* Calcula a Quantidade de Passageiras Na Classe Executiva */
-console.log ("Sendo:", criancaexecutiva, "criança(s) &", adultoexecutiva, "adulto(s)") /* Exibe a Quantidade de Adultos e Crianças Separadamente */
+console.log ("A quantidade de pessoas na classe executiva é:", totalexecutiva) /* Quantidade de Passageiras Na Classe Executiva */
+console.log ("Sendo:", criancaexecutiva, "criança(s) &", adultoexecutiva, "adulto(s)") /* Quantidade de Adultos e Crianças Separadamente*/
 console.log ()
-console.log ("o valor total da viagem é: R$", ((criancaeconomica + adultoeconomica) * 4200) + ((criancaexecutiva + adultoexecutiva) * 6900)) /* Calcula o Valor Total da Viagem */
+console.log ("o valor total da viagem é: R$", total) /* Valor Total da Viagem*/
+console.log ()
+}
+
+else if (regiao == "africa") { /* Determina Condição Caso A Região Seja O Norte do Brasil */
+let total = ((criancaeconomica + adultoeconomica) * 4200) + ((criancaexecutiva + adultoexecutiva) * 6900) /* Calcula o Valor TOtal Da Viagem  */
+console.log ()
+console.log ("A quantidade de Passageiros Adultos é:", totaladulto) /* Quantidade de Passageiras Adultos */
+console.log ()
+console.log ("A quantidade de Passageiros Crianças é:", totalcrianca) /* Quantidade de Passageiras Crianças */
+console.log ()
+console.log ("Destino escolhido:", regiao) /* Exibe Para onde Será a Viagem */
+console.log ()
+console.log ("A quantidade de pessoas na classe econômica é:", totaleconomica) /* Quantidade de Passageiras Na Classe Econômica */
+console.log ("Sendo:", criancaeconomica, "criança(s) &", adultoeconomica, "adulto(s)") /* Quantidade de Adultos e Crianças Separadamente*/
+console.log ()
+console.log ("A quantidade de pessoas na classe executiva é:", totalexecutiva) /* Quantidade de Passageiras Na Classe Executiva */
+console.log ("Sendo:", criancaexecutiva, "criança(s) &", adultoexecutiva, "adulto(s)") /* Quantidade de Adultos e Crianças Separadamente*/
+console.log ()
+console.log ("o valor total da viagem é: R$", total) /* Valor Total da Viagem*/
 console.log ()
 }
